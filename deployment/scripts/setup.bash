f9883095d05c2be7a2920390c595545a5f2a43fc
Ishwor Gurung <me@ishworgurung.com>
2023-11-19 17:28:10 +1100

    fix(kubernetes-events): init

---
+++ deployment/scripts/setup.bash
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+#set -x
+
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CLUSTER_NAME="localdev"
+
+# Setup local registry
+setup_registry() {
+  local CLUSTER_NAME="kind-registry"
+  local reg_name="${CLUSTER_NAME}"
+  local reg_port='5000'
+  local is_running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
+  if [ "${is_running}" != 'true' ]; then
+    docker run \
+      -d --restart=always -p "${reg_port}:5000" --name "${reg_name}" \
+      registry:latest
+  fi
+  kind delete cluster -n "${CLUSTER_NAME}"
+  kind create cluster -n "${CLUSTER_NAME}" --config "${SCRIPTS_DIR}/../manifests/registry-kind-config.yaml"
+  # Connect registry to cluster
+  docker network connect "kind" "${CLUSTER_NAME}" || { echo "Already connected"; }
+  sleep 5
+}
+
+# Setup local kind cluster
+setup_kind() {
+  kind delete cluster -n "${CLUSTER_NAME}"
+  kind create cluster -n "${CLUSTER_NAME}" \
+    --config "${SCRIPTS_DIR}/../manifests/localdev-kind-config.yaml"
+  sleep 5
+  setup_registry
+}
+
+# Deploy kubernetes-events to local kind cluster
+deploy() {
+  setup_kind
+  local IMAGE_NAME="kubernetes-events"
+
+  docker build -t localhost:5000/${IMAGE_NAME}:${IMAGE_TAG} -f deployment/build/Dockerfile .
+  docker push localhost:5000/${IMAGE_NAME}:${IMAGE_TAG}
+  kubectl delete -f deployment/manifests/test-service.yaml --ignore-not-found=true
+  kubectl apply -f deployment/manifests/test-service.yaml
+} && deploy
---
+++ src/main.rs
@@ -0,0 +1,117 @@
+use ansi_term::Color::{Purple, Red, White, RGB};
+use ansi_term::Style;
+use chrono::{DateTime, Local, Utc};
+use fnv::FnvHasher;
+use futures::TryStreamExt;
+use k8s_openapi::api::core::v1::Event;
+use k8s_openapi::apimachinery::pkg::apis::meta::v1::Time;
+use kube::{
+    api::{Api, ResourceExt},
+    runtime::{watcher, WatchStreamExt},
+    Client,
+};
+use std::collections::HashMap;
+use std::hash::Hasher;
+use std::ops::DerefMut;
+use std::sync::{Arc, Mutex};
+
+#[tokio::main]
+async fn main() -> Result<(), watcher::Error> {
+    let client = Client::try_default().await.unwrap();
+    let red_bold_blink: Style = Red.bold().blink();
+    let white_bold: Style = White.bold();
+    let purple_normal = Purple.normal();
+    let focus = RGB(70, 130, 180).bold();
+
+    let seen_events = Arc::new(Mutex::new(HashMap::<u64, bool>::new()));
+
+    let events: Api<Event> = Api::all(client);
+    let w = watcher(events, watcher::Config::default())
+        .applied_objects()
+        .try_for_each(move |e| {
+            let hash_key_clone = String::with_capacity(5);
+
+            // Outer scope. We clone the Arc's to move into the async block
+            let seen_events_clone = seen_events.clone();
+
+            async move {
+                let e_clone = e.clone();
+                let event_ns = e_clone.namespace().unwrap();
+                let time_now: Time = Time(Utc::now());
+                let event_timestamp_utc: &DateTime<Utc> =
+                    &e_clone.first_timestamp.unwrap_or(time_now).0;
+                let event_timestamp_local: DateTime<Local> = DateTime::from(*event_timestamp_utc);
+                let event_source_component = e_clone
+                    .source
+                    .unwrap_or_default()
+                    .component
+                    .unwrap_or_default();
+                let event_msg = e_clone.message.unwrap_or_default();
+                let event_type = e_clone.type_.unwrap();
+                let event_hash: u64 = fnv_hash(
+                    hash_key_clone.clone(),
+                    event_timestamp_local,
+                    event_source_component.clone(),
+                    event_ns.clone(),
+                    event_type.clone(),
+                    event_msg.clone(),
+                );
+
+                let mut seen_events = seen_events_clone.lock().unwrap();
+                if seen_events.contains_key(&event_hash)
+                    && seen_events.get(&event_hash).unwrap() == &true
+                {
+                    println!("dup event fnv: {:x}", event_hash);
+                    return Ok(());
+                }
+                seen_events.insert(event_hash, true);
+
+                #[allow(unused_assignments)]
+                let mut color_style: Style = Style::new();
+                if event_type != "Normal" {
+                    color_style = red_bold_blink;
+                } else {
+                    color_style = white_bold;
+                }
+                let event_reason = e_clone.reason.unwrap_or_default();
+                let event_count = e_clone.count.unwrap_or_default();
+                println!(
+                    "{} {}@{}: {} ({})({})({}) (fnv: {:x})",
+                    purple_normal.paint(event_timestamp_local.to_string()),
+                    event_source_component,
+                    event_ns,
+                    focus.paint(event_msg),
+                    color_style.paint(event_type),
+                    event_reason,
+                    event_count,
+                    event_hash,
+                );
+                Ok(())
+            }
+        });
+
+    w.await?;
+
+    Ok(())
+}
+
+fn fnv_hash(
+    hash_key: String,
+    event_timestamp_local: DateTime<Local>,
+    event_source_component: String,
+    event_ns: String,
+    event_type: String,
+    event_msg: String,
+) -> u64 {
+    let mut hash_key = hash_key.clone().as_str().to_string();
+    let mut fnv_hasher = fnv::FnvHasher::default();
+
+    hash_key.push_str(event_timestamp_local.to_string().as_str());
+    hash_key.push_str(&event_source_component);
+    hash_key.push_str(&event_ns);
+    hash_key.push_str(&event_type);
+    hash_key.push_str(event_msg.as_str());
+
+    fnv_hasher.write(hash_key.as_bytes());
+    fnv_hasher.finish()
+}
